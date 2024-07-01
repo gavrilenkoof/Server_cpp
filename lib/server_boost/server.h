14d412d8bafa581dc04d5d7102d350920f88af18
Ivan Haurylenka <hip@uvr.aero>
2024-07-01 16:07:51 +0300

    add udp boost server

---
+++ lib/server_boost/server.h
@@ -0,0 +1,37 @@
+#pragma once
+
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+
+
+using boost::asio::ip::udp;
+
+
+
+namespace net
+{
+
+
+class udp_server
+{
+public:
+    udp_server(boost::asio::io_context& io_context);
+    ~udp_server();
+private:
+
+    void _start_receive();
+    void _handle_receive(const boost::system::error_code& error, 
+                            std::size_t bytes_transferred);
+
+    void _handle_send(const boost::system::error_code& error,
+                            std::size_t bytes_transferred);
+
+private:
+    udp::socket _socket;
+    udp::endpoint _remote_endpoint;
+    std::array<char, 1> _recv_buffer;
+};
+
+
+}
